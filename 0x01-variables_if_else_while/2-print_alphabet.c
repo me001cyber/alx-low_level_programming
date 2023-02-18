fa644c50d6250d2632775255086f44cdc7587be5
me001cyber <dominoscrazy@gmail.com>
2023-02-18 03:57:53 -0800

    C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,10 @@
  *    *		following by new line using just
  *     *		`putchar` and ASCII numbers.
  *      * Return: Always 0 (success)
- *       */
+ **/
 int main(void)
 {
 		char L;
-
 			for (L = 97; L <= 122; L++)
 						putchar(L);
 				putchar(10);
