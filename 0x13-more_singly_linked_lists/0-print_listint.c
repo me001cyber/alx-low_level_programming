e2bd75009a0adec0550632877f1b1d17d6a40012
me001cyber <dominoscrazy@gmail.com>
2023-04-04 17:01:54 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t numm = 0;
+	size_t nano = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		numm++;
 		h = h->next;
+		nano++;
 	}
 
-	return (numm);
+	return (nano);
 }
